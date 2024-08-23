35c6ef9f3d907e6de88bacdc52361bf7fd2ceb34
Jesse F. Warren V <jfw224@lehigh.edu>
2024-08-23 15:40:21 -0400

    Update web.py

--- web.py
+++ web.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from PIL import Image
 import numpy as np
 import cv2
@@ -15,6 +16,7 @@ import random
 import pandas as pd
 import subprocess
 import os
+import re
 import shutil
 
 def clear_directory(directory_path):
@@ -29,7 +31,7 @@ def clear_directory(directory_path):
             print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 def scrape_image(driver, wait):
-
+    clear_directory('yolov5/data/test_images/tests')
     driver.switch_to.default_content()
     challenge_iframe = wait.until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, 'iframe[title="recaptcha challenge expires in two minutes"]')))   
     image = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'img[src*="api2/payload"]')))
@@ -52,7 +54,7 @@ def scrape_image(driver, wait):
     single_width = width // 3
     single_height = height // 3
 
-    clear_directory('yolov5/data/test_images/tests')
+    #clear_directory('yolov5/data/test_images/tests')
     images = []
     for i in range(3):  # for each row
         for j in range(3):  # for each column
@@ -123,12 +125,140 @@ def run_yolo_on_images(label, images, model_name):
             
         exp = exp + 1
         save_value(exp)
+
+def run_yolo_on_images_2(label, images, model_name):
+    for image in images:
+        # The image_id is extracted from the image filename
+        # Assuming the image filename format is "{label}_part_{index}.png"
+        match = re.search(r'captcha_(\d+)', image.filename)
+        if match:
+            index = int(match.group(1))  # Get the numerical index from the filename
+        else:
+            continue  # If the pattern is not found, skip this image
+
+        image_id = f"redo_part_{index}"
+        image_path = os.path.join("yolov5/data/test_images/tests/", f"{image_id}.png")
+        image.save(image_path)
+        image_id = f"redo_part_{index}.png"
         
+        # Construct the command
+        command = [
+            "python", "detection.py", 
+            image_id,
+            model_name
+        ]
+
+        # Execute the command
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        # Run the shell script for each image
+
+        print(result.stdout)
+        print(result.stderr)
+
+        image_id = f"redo_part_{index}"
+        exp = load_value()
+        file_path = f"yolov5/runs/detect/exp{exp}/labels/{image_id}.txt"
+        if os.path.exists(file_path):  # Was something detected in the image?
+            copy_label_file(image_id, exp)
+            # Determine if the specified object is detected in the image
+            if object_detected_in_bbx('label.txt', label):
+                select_image_in_selenium_2(index)
+            
+        exp += 1
+        save_value(exp)
+        
+def crop_image_from_screenshot(save_path, i, grid_size=(3, 3)):
+    # Open the full screenshot
+    full_screenshot_path = f"{save_path}/captcha_screenshot_{i}.png"
+    img = Image.open(full_screenshot_path)
+    
+    # Calculate the size of each tile
+    width, height = img.size
+    img_without_header = img.crop((0, 120, width, height))
+    
+
+    # Recalculate the height without the header
+    height -= 120
+
+    tile_width = width // grid_size[0]
+    tile_height = height // grid_size[1]
+    
+    # Calculate row and column index based on the linear index 'i'
+    row_index = i // grid_size[0]
+    col_index = i % grid_size[0]
+    
+    # Calculate the position to start cropping
+    left = col_index * tile_width
+    upper = row_index * tile_height
+    right = left + tile_width
+    lower = upper + tile_height
+    
+    # Crop the image
+    cropped_image = img_without_header.crop((left, upper, right, lower))
+    
+    # Save or handle the cropped image as needed
+    cropped_image_path = f"yolov5/data/test_images/tests3/cropped_captcha_{i}.png"
+    cropped_image.save(cropped_image_path)
+
+def crop_image_from_screenshot_2(save_path, i, grid_size=(3, 3)):
+    # Open the full screenshot
+    full_screenshot_path = f"{save_path}/captcha_screenshot_{i}.png"
+    img = Image.open(full_screenshot_path)
+    
+    # Calculate the size of each tile
+    width, height = img.size
+    img_without_header = img.crop((0, 120, width, height))
+    
+
+    # Recalculate the height without the header
+    height -= 120
+
+    tile_width = width // grid_size[0]
+    tile_height = height // grid_size[1]
+    
+    # Calculate row and column index based on the linear index 'i'
+    row_index = i // grid_size[0]
+    col_index = i % grid_size[0]
+    
+    # Calculate the position to start cropping
+    left = col_index * tile_width
+    upper = row_index * tile_height
+    right = left + tile_width
+    lower = upper + tile_height
+    
+    # Crop the image
+    cropped_image = img_without_header.crop((left, upper, right, lower))
+    
+    # Save or handle the cropped image as needed
+    cropped_image_path = f"yolov5/data/test_images/tests4/cropped_captcha_{i}.png"
+    cropped_image.save(cropped_image_path)
+
+
+def take_screenshot_with_selenium(driver, save_path, i):
+    # Wait for the reCAPTCHA images to be visible
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'img[src*="api2/payload"]'))
+    )
+    # Locate the reCAPTCHA element
+    captcha_element = driver.find_element(By.CSS_SELECTOR, 'div.rc-imageselect-payload')
+    # Take screenshot of just the reCAPTCHA element
+    captcha_element.screenshot(f"{save_path}/captcha_screenshot_{i}.png")
 
 def select_image_in_selenium(image_index):
     # Code to click the appropriate image in the captcha based on index
     elements = driver.find_elements(By.CLASS_NAME, 'rc-imageselect-tile')
     elements[image_index].click()
+    time.sleep(7)
+    take_screenshot_with_selenium(driver, 'yolov5/data/test_images/tests2', image_index)
+    crop_image_from_screenshot('yolov5/data/test_images/tests2', image_index)
+
+def select_image_in_selenium_2(image_index):
+    # Code to click the appropriate image in the captcha based on index
+    elements = driver.find_elements(By.CLASS_NAME, 'rc-imageselect-tile')
+    elements[image_index].click()
+    time.sleep(7)
+    take_screenshot_with_selenium(driver, 'yolov5/data/test_images/tests2', image_index)
+    crop_image_from_screenshot_2('yolov5/data/test_images/tests2', image_index)
 
 def object_detected_in_bbx(bbx_file_path, target_class):
     detected = False
@@ -139,8 +269,20 @@ def object_detected_in_bbx(bbx_file_path, target_class):
         num = 1
     elif target_class == "crosswalks":
         num = 0
-    else:
-        num = -1
+    elif target_class == "cars":
+        num = 3
+    elif target_class == "bus":
+        num = 4
+    elif target_class == "bicycles":
+        num = 5
+    elif target_class == "a fire hydrant":
+        num = 6
+    elif target_class == "bridges":
+        num = 7
+    elif target_class == "motorcycles":
+        num = 8
+    elif target_class == "trafficlights":
+        num = 9
     with open(bbx_file_path, 'r') as file:
         for line in file:
             elements = line.split()
@@ -149,6 +291,64 @@ def object_detected_in_bbx(bbx_file_path, target_class):
                 break
 
     return detected
+
+def scrape_new_images(driver, wait, directory_path):
+    # Ensure the directory is ready to store new images
+    clear_directory(directory_path)
+
+    image = EC.presence_of_element_located((By.CSS_SELECTOR, 'img[src*="api2/payload"]'))
+    image_src = image.get_attribute('src')
+    class_name = image.get_attribute('class')
+
+
+    label = driver.find_element(By.TAG_NAME, 'strong').text
+
+    response = requests.get(image_src)
+
+    image_content = response.content
+
+        # Open the image from bytes
+    image = Image.open(BytesIO(image_content))
+
+    # Calculate dimensions for the smaller images
+
+    # Process each new image
+    images = []
+    label = driver.find_element(By.TAG_NAME, 'strong').text
+    for img in images:
+        image_src = img.get_attribute('src')
+        response = requests.get(image_src)
+        image_content = response.content
+        image = Image.open(BytesIO(image_content))
+        width, height = image.size
+        single_width = width // 3
+        single_height = height // 3
+
+        for i in range(3):  # for each row
+            for j in range(3):  # for each column
+                left = j * single_width
+                top = i * single_height
+                right = left + single_width
+                bottom = top + single_height
+                cropped_image = image.crop((left, top, right, bottom))
+                file_name = f"{label}_part_{i * 3 + j}.png"
+                file_path = os.path.join(directory_path, file_name)
+                cropped_image.save(file_path)
+                images.append(cropped_image)
+    
+    return images, label
+
+def load_images_array(directory_path):
+    # List all files in the given directory
+    all_files = os.listdir(directory_path)
+    
+    # Filter out files with image extensions
+    image_files = [file for file in all_files if file.endswith(('.png', '.jpg', '.jpeg'))]
+    
+    # Load the image files into PIL Images
+    images = [Image.open(os.path.join(directory_path, file)) for file in image_files]
+    
+    return images
 
 captcha_site = 'https://www.google.com/recaptcha/api2/demo'
 click_delay = .5
@@ -170,14 +370,28 @@ recaptcha_wait = WebDriverWait(driver, 10)
 recaptcha = recaptcha_wait.until(EC.presence_of_element_located((By.ID, 'rc-anchor-container')))
 recaptcha.click()
 
-
+clear_directory('yolov5/data/test_images/tests2')
+clear_directory('yolov5/data/test_images/tests3')
 images, label = scrape_image(driver, wait)
 
-run_yolo_on_images(label, images, 'model5')
+run_yolo_on_images(label, images, 'modelA1')
 
-images, label = scrape_image(driver, wait)
+directory_path = 'yolov5/data/test_images/tests3'
+images2 = load_images_array(directory_path)
 
-run_yolo_on_images(label, images, 'model5')
+run_yolo_on_images_2(label, images2, 'modelA1')
+
+directory_path = 'yolov5/data/test_images/tests4'
+images2 = load_images_array(directory_path)
+
+run_yolo_on_images_2(label, images2, 'modelA1')
+
+#new_images_directory = 'yolov5/data/test_images/tests2'  # Different directory for new images
+#images2, label2 = scrape_image(driver, wait)
+
+#run_yolo_on_images(label2, images2, 'modelA1')
+
+time.sleep(1)
 
 verify_button = driver.find_element(By.ID, 'recaptcha-verify-button')
 verify_button.click()
